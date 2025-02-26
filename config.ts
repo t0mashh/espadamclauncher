759b5795aadb668d30d83d52f5e37da01444e57c
t0mashh <129892614+t0mashh@users.noreply.github.com>
2025-02-26 12:25:59 +0200

    Update config.ts

--- config.ts
+++ config.ts
@@ -7,12 +7,12 @@ export const window = {
     resizable: false,
     maximizable: false,
     fullscreenable: false,
-    title: 'Aurora Launcher',
+    title: 'Espada Launcher',
 };
 
 export const api = {
-    ws: 'ws://127.0.0.1:1370/ws',
-    web: 'http://127.0.0.1:1370',
+    ws: 'wss://launcher.espadateam.xyz/ws',
+    web: 'https://launcher.espadateam.xyz',
     // extraToken: token,
 };
 
